c1dc768f03b8145e4dd3bcada8fc888b843c641c
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-06-17 15:00:37 +0900

    add hooks

---
+++ frontend/src/hooks/useGenerateSchedule.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export function useGenerateschedule() {
+  const [schedule, setSchedule] = useState("");
+
+  const fetchSchedule = () => {
+    const prompt = "来週までに、10kg太りたいです。月曜日の予定を一時間ごとに記載して24時間分になるようにしてください。なるべく具体的に書いて。例えば、ご飯だったらメニュー、出かけるんだったら場所まで。json形式で返して"
+    const url = `http://127.0.0.1:5000/chat?prompt=${encodeURIComponent(prompt)}`;
+
+    fetch(url)
+      .then((response) => setSchedule(response)) // レスポンスを Blob として解釈
+      .catch((error) => console.error(error));
+  }
+
+
+  return {
+    schedule,
+    fetchSchedule
+  }
+}
