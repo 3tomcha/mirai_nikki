099ad79c782fa5054db722150d4d71b328d6c5aa
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-06-18 20:52:13 +0900

    succeeded to participate

--- frontend/src/hooks/useContract.tsx
+++ frontend/src/hooks/useContract.tsx
@@ -1,13 +1,16 @@
 import { ethers } from 'ethers';
 import { useState } from 'react';
 import { GoalContract } from '../../types/ethers-contracts/GoalContract';
+import { GoalToken } from '../../types/ethers-contracts/GoalToken';
 import GoalContractAbi from "../abi/GoalContract.json"
+import GoalTokenAbi from "../abi/GoalToken.json"
 
 export default function useContract() {
   const [ethereum, setEthereum] = useState<any>();
   const [accounts, setAccounts] = useState<any>();
   const [success, setSuccess] = useState<boolean>(false);
   const goalContractAddress = "0x48395e3F6F183E7216957A1c0bABAA3967Ed8c20"
+  const goalTokenAddress = "0x94391Aa1286d68911Aa11512778bFB1cd4674Cf7"
 
   const init = () => {
     if ((window as any).ethereum) {
@@ -31,8 +34,14 @@ export default function useContract() {
     console.log(provider);
     const signer = await provider.getSigner();
     const goalContract = new ethers.Contract(goalContractAddress, GoalContractAbi, signer) as unknown as GoalContract;
+    const goalToken = new ethers.Contract(goalTokenAddress, GoalTokenAbi, signer) as unknown as GoalToken;
     // approveする
-    await goalContract.participate(1);
+    await goalToken.approve(goalContractAddress, 1).catch((err) => {
+      console.log(err);
+    });
+    await goalContract.participate(1).catch((err) => {
+      console.log(err);
+    });
   }
 
   return {
